4d666494fd4dc3c5715d9ee2f4d5d4be1209b824
eamcalister <mcalister_e@fortlewis.edu>
2026-08-24 13:25:57 -0600

    Latest build deployed.

--- worksheets/lunr-pretext-search-index.js
+++ worksheets/lunr-pretext-search-index.js
@@ -1,13 +1,58 @@
 var ptx_lunr_search_style = "textbook";
 var ptx_lunr_docs = [
 {
-  "id": "sec-section-name",
+  "id": "frontmatter",
   "level": "1",
-  "url": "sec-section-name.html",
-  "type": "Article",
+  "url": "frontmatter.html",
+  "type": "Front Matter",
   "number": "",
-  "title": "In-Class Activities",
-  "body": " In-Class Activities    Mathematics Department Fort Lewis College  Last Updated:         These activities are designed to be used in class to help students understand the material. They are not graded, but students are encouraged to work on them in groups and ask the instructor for help when needed.     "
+  "title": "Front Matter",
+  "body": "  Mathematics Department Fort Lewis College  Last Updated:       "
+},
+{
+  "id": "Activity-title",
+  "level": "1",
+  "url": "Activity-title.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Math 325 Homework 1",
+  "body": " Math 325 Homework 1   Due August 28, 2026 (submission on Canvas).     The Babylonian tablet YBC7289 gives an approximation for as . Calculate this as an exact fraction (noting how you know order of magnitude) in our modern number system. When you plug this into a caclulator, how accurate is it?      The goal of this problem is to possibly make better sense of Babylonian reciprocals by thinking about our own number system without zero or explicit place values.     Describe how our number system would work if we used the symbols . (Your answer should be maybe a couple short paragraphs, with some examples of numbers written normally and not.)      In the number system from above, find the reciprocals of , , , and .       Find the Babylonian reciprocal of .      Do you think mathematics (what existed at the time) was really accessible to the masses in ancient Mespotamia? Do you think it was more or less accesible than mathematics of today? Explain.    "
+},
+{
+  "id": "Activity-title-3",
+  "level": "2",
+  "url": "Activity-title.html#Activity-title-3",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  The Babylonian tablet YBC7289 gives an approximation for as . Calculate this as an exact fraction (noting how you know order of magnitude) in our modern number system. When you plug this into a caclulator, how accurate is it?   "
+},
+{
+  "id": "Activity-title-4",
+  "level": "2",
+  "url": "Activity-title.html#Activity-title-4",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  The goal of this problem is to possibly make better sense of Babylonian reciprocals by thinking about our own number system without zero or explicit place values.     Describe how our number system would work if we used the symbols . (Your answer should be maybe a couple short paragraphs, with some examples of numbers written normally and not.)      In the number system from above, find the reciprocals of , , , and .    "
+},
+{
+  "id": "Activity-title-5",
+  "level": "2",
+  "url": "Activity-title.html#Activity-title-5",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Find the Babylonian reciprocal of .   "
+},
+{
+  "id": "Activity-title-6",
+  "level": "2",
+  "url": "Activity-title.html#Activity-title-6",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  Do you think mathematics (what existed at the time) was really accessible to the masses in ancient Mespotamia? Do you think it was more or less accesible than mathematics of today? Explain.   "
 }
 ]
 
